5975bec86e66e3230cccbac327fca62343f5a58d
Gerard Rodes <grv94sk@gmail.com>
2016-10-05 17:44:10 +0200

    Add files via upload

--- Xml2json.py
+++ Xml2json.py
@@ -2,36 +2,36 @@ import xml.etree.ElementTree as ET
 
 class Xml2json:
 
-	def __init__(self, filePath):
-		self.tree = ET.parse(filePath)
-		self.json = self.parseXml(self.tree.getroot(), {})
-		
-		
-		
-	def parseXml(self, treeElement, json, isRoot = True):
-		
-		element = {}
-		
-		for attr in treeElement.attrib:
-			element['@'+attr] = treeElement.attrib[attr]
-		
-		
-		childDone = []
-		
-		for children in treeElement:
-			if children.tag not in childDone:
-				childDone.append(children.tag)
-				siblings = treeElement.findall(children.tag)
-				
-				if len(siblings) > 1:
-					element[children.tag] = []
-					for i, sibling in enumerate(siblings):
-						element[children.tag].append({})
-						element[children.tag][i] = self.parseXml(sibling, element[children.tag][i], isRoot=False)
-				else:
-					element[children.tag] = children.text
+  def __init__(self, filePath):
+    self.tree = ET.parse(filePath)
+    self.json = self.parseXml(self.tree.getroot(), {})
+    
+    
+    
+  def parseXml(self, treeElement, json, isRoot = True):
+    
+    element = {}
+    
+    for attr in treeElement.attrib:
+      element['@'+attr] = treeElement.attrib[attr]
+    
+    
+    childDone = []
+    
+    for children in treeElement:
+      if children.tag not in childDone:
+        childDone.append(children.tag)
+        siblings = treeElement.findall(children.tag)
+        
+        if len(siblings) > 1:
+          element[children.tag] = []
+          for i, sibling in enumerate(siblings):
+            element[children.tag].append({})
+            element[children.tag][i] = self.parseXml(sibling, element[children.tag][i], isRoot=False)
+        else:
+          element[children.tag] = children.text
 
-		if isRoot:
-			return {treeElement.tag: element}
-		else:
-			return element
+    if isRoot:
+      return {treeElement.tag: element}
+    else:
+      return element
